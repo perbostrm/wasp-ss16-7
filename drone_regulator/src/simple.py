46e0454e2d17a9a4ba50797d49482d480484bdd0
Per Skarin <per.skarin@gmail.com>
2016-08-25 06:45:15 +0000

    zengine

--- drone_regulator/src/simple.py
+++ drone_regulator/src/simple.py
@@ -3,27 +3,59 @@ import rospy
 from std_msgs.msg import String
 from ardrone_autonomy.msg import Navdata
 from geometry_msgs.msg import Twist
+from velctrl import Step,Cont,Pulse
 
 pub = None
+battery = 20
+hastag = False
+engz = Cont()
+engx = Cont()
 
-def callback(data):
+def cb_navdata(data):
   global pub
+  global battery
+  global engz
+  global hastag
+
+  if int(data.batteryPercent/5) < battery:
+    battery = int(data.batteryPercent/5)
+    rospy.loginfo("Battery: {}-{}%".format(battery*5, (battery+1)*5))
+  
   if data.tags_count > 0:
+    hastag = True
     rospy.loginfo("Tag found {} {} {}".format(data.tags_xc[0], data.tags_yc[0], data.tags_orientation[0]))
-    e = data.tags_orientation[0]
-    d = -e
-    vel = Twist()
-    vel.linear.x = 0;
-    vel.linear.y = 0;
-    vel.linear.z = 0;
-    vel.angular.x = 0;
-    vel.angular.y = 0;
-    vel.angular.z = d/360;
+    if engz.ready():
+      e = 180-data.tags_orientation[0]
+      if e > 5:
+        engz.act(-2)
+      elif e < -5:
+        engz.act(2)
+      else:
+        engz.act(0)
+  elif hastag and engz.ready():
+    hastag = False
+    engz.act(0)
+
+
+  sendcmd = False
+  vel = Twist()
+  vel.linear.x = 0;
+  vel.linear.y = 0;
+  vel.linear.z = 0;
+  vel.angular.x = 0;
+  vel.angular.y = 0;
+  vel.angular.z = 0;
+
+  if engz.next():
+    sendcmd = True
+    vel.angular.z = engz.val()
+  
+  if sendcmd: 
     rospy.loginfo(vel)
     pub.publish(vel)
-    
 def listener():
   global pub
+
   pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
 
   # In ROS, nodes are uniquely named. If two nodes with the same
@@ -33,9 +65,9 @@ def listener():
   # run simultaneously.
   rospy.init_node('ardrone_ctrl', anonymous=True)
 
-  rospy.Subscriber("ardrone/navdata", Navdata, callback)
+  rospy.Subscriber("ardrone/navdata", Navdata, cb_navdata)
 
-  rospy.loginfo("GO!")
+#  rospy.loginfo("GO!")
   # spin() simply keeps python from exiting until this node is stopped
   rospy.spin()
 
