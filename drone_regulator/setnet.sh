ea227c8d382ecd6aad314f2410ee7f922c88c2a2
Per Skarin <per.skarin@gmail.com>
2016-08-26 07:34:03 +0000

    Change ip add tut_ardrone

--- drone_regulator/setnet.sh
+++ drone_regulator/setnet.sh
@@ -8,7 +8,7 @@ function die()
 
 echo "/data/wifi.sh" | telnet 192.168.1.1;
 echo $?;
-while ! ping -c 1 -W 1 192.168.1.10 >/dev/null 2>&1;
+while ! ping -c 1 -W 1 192.168.1.207 >/dev/null 2>&1;
 do
 	echo "Waiting for drone... (now you need to connect to wasp_7)"
 done;
